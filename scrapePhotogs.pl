41d72d056f5bf5467686df62dfcb9ba0562b950b
Mother-Screng <gedanken@edgeoftheworld.us>
2023-03-22 10:30:42 -0400

    Added Hashes for Locations and Based-In both, solid progress

--- scrapePhotogs.pl
+++ scrapePhotogs.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
+use warnings;
 use utf8;
 
 require v5.12;
@@ -57,11 +58,15 @@ use IO::Socket::SSL qw();
 
 my $mainURL = "https://www.the-aop.org/find/photographers?specialism=&locations=&base=&awards=&video_stills=&displayMode=list&letter=";
 
-my $myLoc;     # Variables to be re-used to build one persons hash
-my $myBased;
+my $name = "";
+my $myLoc = "";     # Variables to be re-used to build one persons hash
+my $myBased = "";   # This seems dumb to set to empty quotes but it is necessary for use in anon hashes below such as $places. Uhh why are those hashes anon?? -- MATT CHECK
 
-my places = {$myLoc, $myBased};
-my person = {$name, $places}; # Places is a (loc, based) hash?
+my $places = {$myLoc, $myBased};
+my $person = {$name, $places}; # Places is a (loc, based) hash?
+
+# my $locsRef;
+# my $basedInRef;
 
 sub grabMainPage {
 
@@ -88,7 +93,50 @@ sub grabMainPage {
 
 sub grabLocs {
 
-   # LAZY - i need to re re-write to generate dynamically
+   # LAZY - i need to re re-write to generate dynamically by scraping!!!
+   
+   # <option value="1">Anywhere in the UK (403)</option>
+   
+   # <option value="2">East Midlands (34)</option>
+   # <option value="3">East of England (39)</option>
+   # <option value="4">London & the South East (483)</option>
+   # <option value="5">Manchester & the North West (44)</option>
+   # <option value="6">North East (27)</option>
+   # <option value="7">Northern Ireland (30)</option>
+   # <option value="8">Scotland (34)</option>
+   # <option value="9">South West (36)</option>
+   # <option value="10">Wales (29)</option>
+   # <option value="11">West Midlands (26)</option>
+   # <option value="12">Yorkshire & Humberside (28)</option>
+   # <option value="14">North Wales (20)</option>
+   # <option value="15">Worldwide (311)</option>
+   # <option value="16">Europe (135)</option>
+   # <option value="17">Overseas (98)</option>
+   # <option value="18">Ireland (40)</option>
+   
+   my %locs = (
+      1 => "Anywhere in the UK",
+      2 => "East Midlands",
+      3 => "East of England",
+      4 => "London & the South East",
+      5 => "Manchester & the North West",
+      6 => "North East",
+      7 => "Northern Ireland",
+      8 => "Scotland",
+      9 => "South West",
+      10 => "Wales",
+      11 => "West Midlands",
+      12 => "Yorkshire & Humberside",
+      14 => "North Wales",
+      15 => "Worldwide",
+      16 => "Europe".
+      17 => "Overseas",
+      18 => "Ireland",
+   );
+   
+   my $hashref = \%locs;
+   
+   return $hashref;
     
 }
 
@@ -96,6 +144,60 @@ sub grabBasedIn {
     
     # LAZY - i need to re re-write to generate dynamically
     
+    # <option value="1">Republic of Ireland (18)</option>
+    # <option value="4">London (415)</option>
+    # <option value="5">Wales (7)</option>
+    # <option value="6">Scotland (12)</option>
+    # <option value="7">Yorkshire & Humberside (18)</option>
+    # <option value="8">West Midlands (11)</option>
+    # <option value="9">East Midlands (12)</option>
+    # <option value="10">South East (40)</option>
+    # <option value="11">South West (30)</option>
+    # <option value="12">North West (17)</option>
+    # <option value="13">North East (5)</option>
+    # <option value="14">East of England (8)</option>
+    # <option value="15">Northern Ireland (1)</option>
+    # <option value="16">USA (5)</option>
+    # <option value="19">Australia (2)</option>
+    # <option value="23">France (4)</option>
+    # <option value="24">Germany (21)</option>
+    # <option value="25">Netherlands (3)</option>
+    # <option value="28">Spain (2)</option>
+    # <option value="40">Guernsey (2)</option>
+    # <option value="41">Hong Kong (2)</option>
+    # <option value="58">South Africa (1)</option>
+    # <option value="71">UK/Europe/USA/Asia (4)</option>
+    
+    my %basedIn = (
+      '1' => 'Republic of Ireland',
+      '4' => 'London',
+      '5' => 'Wales',
+      '6' => 'Scotland',
+      '7' => 'Yorkshire & Humberside',
+      '8' => 'West Midlands',
+      '9' => 'East Midlands',
+      '10' => 'South East',
+      '11' => 'South West',
+      '12' => 'North West',
+      '13' => 'North East',
+      '14' => 'East of England',
+      '15' => 'Northern Ireland',
+      '16' => 'USA'.
+      '19' => 'Australia',
+      '23' => 'France',
+      '24' => 'Germany',
+      '25' => 'Netherlands',
+      '28' => 'Spain',
+      '40' => 'Guernsey',
+      '41' => 'Hong Kong',
+      '58' => 'South Africa',
+      '71' => 'UK/Europe/USA/Asia',
+      
+   );
+   
+   my $hashref = \%basedIn;
+   
+   return $hashref;
     
 } 
 
@@ -105,7 +207,18 @@ sub nextPage {
 
 # MAIN
 
-grabMainPage()
+grabMainPage();
+
+my $locsRef;
+$locsRef = grabLocs();
+
+# Beware it returns a hashref that needs to be dereferenced such as  $locsRef->{key}
+
+my $basedInRef;
+$basedInRef = grabBasedIn();
+
+# print $locsRef->{1};
+
 
 ;
 
